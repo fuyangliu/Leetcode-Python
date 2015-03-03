f4c960065ffa52b508968fe7b542b8a98ccf829a
Fuyang Liu <fuyangliu@172-16-9-154.dynapool.nyu.edu>
2015-03-03 18:12:39 -0500

    two Sum
    /Users/fuyangliu/Documents/PythonWorkSpace/twoSunHash.py

---
+++ twoSunHash.py
@@ -0,0 +1,10 @@
+def twoSum(num, target):
+    dict = {}
+    for i in xrange(len(num)):
+        x = num[i]
+        if target-x in dict:
+            return (dict[target-x]+1, i+1)
+        dict[x] = i
+        print dict
+
+print twoSum([1,2,3,5],5)
